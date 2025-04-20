40135032d6fe56d927c1e8b9bf9e0f38294e7949
notcis <notcis07@gmail.com>
2025-04-20 09:59:36 +0700

    mobile menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 export default function Header() {
   return (
@@ -23,19 +21,7 @@ export default function Header() {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button variant="ghost" asChild>
-            <Link href="/cart">
-              <ShoppingCartIcon />
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
