c59cc1d338bdbe99203162d614e982e264b4e096
notcis <notcis07@gmail.com>
2025-04-24 20:43:14 +0700

    finnish sign in

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSigninForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+export default async function SignInPage() {
+  const session = await auth();
+  if (session) return redirect("/");
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              height={100}
+              width={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSigninForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
