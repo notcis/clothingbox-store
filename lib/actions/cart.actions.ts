a5cecbd3e5f672272ae7ebaf01524275f1ad981b
notcis <notcis07@gmail.com>
2025-04-28 19:41:45 +0700

    fix eslint

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,6 +2,7 @@
 
 import { CartItem } from "@/types";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function addItemToCart(data: CartItem) {
   return {
     success: true,
