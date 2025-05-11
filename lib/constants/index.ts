807390181124df197434090f426ffe9b7a1e9ae3
notcis <notcis07@gmail.com>
2025-05-11 10:01:15 +0700

    cart page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,7 +1,6 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Clothing Box";
 export const APP_DESCRIPTION =
-  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "A modern ecommerce platform built with Next.js";
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "clothingbox2018";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
